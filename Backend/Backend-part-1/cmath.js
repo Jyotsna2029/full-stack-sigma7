1ee90838c75c905a44f17fbad871704ab2513577
Jyotsna2029 <chauhanjyotsna049@gmail.com>
2025-09-11 00:17:06 +0530

    Delete Backend/Backend-part-1 directory

--- Backend/Backend-part-1/cmath.js
+++
@@ -1,8 +0,0 @@
-const someVal = require("./cExport"); // use relative path
-console.log(someVal);
-
-// Example usage
-console.log("Sum:", someVal.sum(8, 17)); // 26
-console.log("Mul:", someVal.mul(8, 1)); // 8
-console.log("g:", someVal.g);           // 9.8
-console.log("Pi:", someVal.Pi);         // 3.14
--- Backend/Backend-part-1/escript.js
+++
@@ -1,4 +0,0 @@
-import {sum, pi} from "./emath.js";
-
-console.log(sum(1,2));
-console.log(3*pi);
